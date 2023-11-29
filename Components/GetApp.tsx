bbe478eb7c52ce3b2a875a841a52af4f71ef1a41
Saqlain Kazmi <52085112+SaqlainKazmi@users.noreply.github.com>
2023-11-30 02:20:13 +0500

    First and last

---
+++ Components/GetApp.tsx
@@ -0,0 +1,48 @@
+import Button from "./Button";
+import dynamic from "next/dynamic";
+
+const DynamicImage = dynamic(() => import("next/image"), { ssr: false });
+
+
+const GetApp = () => {
+  return (
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get the free now!
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on iOS and Android
+          </p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Button
+              type="button"
+              title="Download App"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+            <Button
+              type="button"
+              title="Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
+            />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <DynamicImage
+            src="/phones.png"
+            alt="phone"
+            width={550}
+            height={870}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default GetApp
